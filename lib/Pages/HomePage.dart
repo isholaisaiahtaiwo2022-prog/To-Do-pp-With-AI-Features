fd020b38635245b3301173a6940039daef915adf
isholaisaiahtaiwo2022-prog <isholaisaiahtaiwo2022@gmail.com>
2026-08-02 20:37:19 +0100

    First Commit

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:to_doapp/Pages/Utilitis/To-Do_File.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text("TO DO"),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          todoTile()
+        ],
+      ),
+    );
+  }
+}
